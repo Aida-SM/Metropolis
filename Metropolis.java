9b8d0e625ef73e1ad200656cd54874a8cab8e026
Aida Sabagh <aida.sabbagh@gmail.com>
2019-01-17 21:43:50 -0500

    define a char variable that is ready for input y/n and replace in conditions

--- Metropolis.java
+++ Metropolis.java
@@ -1,48 +1,31 @@
 import java.util.Scanner;
 public class Metropolis{
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		boolean isCapital;
-		int numberOfCitizens;
-		double taxesPerCitizen;
+        boolean isCapital;
+        char AnswerOfIsCapital;
+        int numberOfCitizens;
+        double taxesPerCitizen;
 
-		Scanner.scanner=new Scanner(System.in);
-		
-		System.out.println("is the city Capital y/n");
-		isCapital=scanner.nextBoolean();
-			if (isCapital=='y'|| isCapital=='Y') {
-					isCapital=1;
-				}
-			else if(isCapital=='n'|| isCapital=='N') {
-					isCapital=0;
+        Scanner scanner = new Scanner(System.in);
 
-				}
-			else
-					System.out.println("is the city Capital y/n");
+        System.out.println("is the city Capital y/n");
+        AnswerOfIsCapital = scanner.next().charAt(0);
+        if (AnswerOfIsCapital == 'y' || AnswerOfIsCapital == 'Y') {
+            isCapital = true;
+        } else if (AnswerOfIsCapital == 'n' || AnswerOfIsCapital == 'N') {
+            isCapital = false;
 
-		System.out.println("How many citizen it has?");
-		numberOfCitizens=scanner.nextInt();
+        } else
+            System.out.println("is the city Capital y/n");
 
-		System.out.println("How much the average income of a year");
-			taxesPerCitizen=scanner.nextDouble();
-			
+        System.out.println("How many citizen it has?");
+        numberOfCitizens = scanner.nextInt();
+
+        System.out.println("How much the average income of a year");
+        taxesPerCitizen = scanner.nextDouble();
 
 
-        
-		
-	}
+    }
 
-
-
-
-
-
-}
-		
-	
-
-
-
-
-
-
+ }
