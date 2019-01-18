f9ba39d1b8e15d351f5ea30b910880ee0c110036
Aida Sabagh <aida.sabbagh@gmail.com>
2019-01-17 20:41:10 -0500

    writing messeges for user to enter input

--- Metropolis.java
+++ Metropolis.java
@@ -5,8 +5,11 @@ public class Metropolis{
 		int numberOfCitizens;
 		double taxesPerCitizen;
 		Scanner.scanner=new Scanner(System.in);
+		System.out.println("is the city Capital y/n");
 		isCapital=scanner.nextBoolean();
+		System.out.println("How many citizen it has?");
 		numberOfCitizens=scanner.nextInt();
+		System.out.println("How much the average income of a year");
 		taxesPerCitizen=scanner.nextDouble();
 
 		
