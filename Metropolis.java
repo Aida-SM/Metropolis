7085991b3eac872d70c1848da54ed0e5aeda05b7
Aida Sabagh <aida.sabbagh@gmail.com>
2019-01-17 23:06:09 -0500

    define a variable for the income of city each year

--- Metropolis.java
+++ Metropolis.java
@@ -7,6 +7,7 @@ public class Metropolis{
         int numberOfCitizens;
         double taxesPerCitizen;
         double incomePerCitizen;
+        double incomeOfCityPerYear;
 
         Scanner scanner = new Scanner(System.in);
 
