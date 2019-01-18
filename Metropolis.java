b3091728aac577fbf3ed486feabacb5496d4df91
Aida Sabagh <aida.sabbagh@gmail.com>
2019-01-17 20:21:19 -0500

    become ready for define input parameters

--- Metropolis.java
+++ Metropolis.java
@@ -1,8 +1,10 @@
+import java.util.Scanner;
 public class Metropolis{
 	public static void main(String[] args) {
 		boolean isCapital=false;
 		int numberOfCitizens;
 		double taxesPerCitizen;
+		Scanner.scanner=new Scanner(System.in);
 		
 	}
 
