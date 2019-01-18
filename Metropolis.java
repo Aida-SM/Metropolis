8a1a33fee67cd01a7223f376cfc8281c4034db0c
Aida Sabagh <aida.sabbagh@gmail.com>
2019-01-17 21:10:54 -0500

    Check the input of boolean "isCapital" parameter

--- Metropolis.java
+++ Metropolis.java
@@ -1,17 +1,34 @@
 import java.util.Scanner;
 public class Metropolis{
 	public static void main(String[] args) {
+
 		boolean isCapital;
 		int numberOfCitizens;
 		double taxesPerCitizen;
+
 		Scanner.scanner=new Scanner(System.in);
+		
 		System.out.println("is the city Capital y/n");
 		isCapital=scanner.nextBoolean();
+			if (isCapital=='y'|| isCapital=='Y') {
+					isCapital=1;
+				}
+			else if(isCapital=='n'|| isCapital=='N') {
+					isCapital=0;
+
+				}
+			else
+					System.out.println("is the city Capital y/n");
+
 		System.out.println("How many citizen it has?");
 		numberOfCitizens=scanner.nextInt();
-		System.out.println("How much the average income of a year");
-		taxesPerCitizen=scanner.nextDouble();
 
+		System.out.println("How much the average income of a year");
+			taxesPerCitizen=scanner.nextDouble();
+			
+
+
+        
 		
 	}
 
